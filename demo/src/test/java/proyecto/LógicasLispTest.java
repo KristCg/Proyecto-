7f66a43cbd6e4596abaa6d533d232864ee2960e2
KristCg <136038238+KristCg@users.noreply.github.com>
2025-03-24 01:30:24 -0600

    Creación de test de la clase LogicasLisp

---
+++ demo/src/test/java/proyecto/LógicasLispTest.java
@@ -0,0 +1,40 @@
+package proyecto;
+
+import org.junit.Test;
+inoort static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+public class LogicasLispTest {
+    Tokenizer tokenizer = new Tokenizer();
+    LogicasLisp logica = new LogicasLisp(tokenizer);
+
+    @Test
+    public void testAND() {
+        assertEquals((Integer) 3, logica.interpretar("(AND 1 2 3)")); // Último valor
+        assertEquals("NIL", logica.interpretar("(AND 1 NIL 3)")); // NIL en la operación
+        assertEquals("NIL", logica.interpretar("(AND NIL NIL NIL)")); // Todos NIL
+        assertEquals("NIL", logica.interpretar("(AND)")); // AND vacío
+    }
+
+    @Test
+    public void testOR() {
+        assertEquals((Integer) 2, logica.interpretar("(OR NIL 2 3)")); // Primer valor verdadero
+        assertEquals((Integer) 1, logica.interpretar("(OR 1 2 3)")); // Primer operando no NIL
+        assertEquals("NIL", logica.interpretar("(OR NIL NIL NIL)")); // Todos NIL
+        assertEquals("NIL", logica.interpretar("(OR)")); // OR vacío
+    }
+
+    @Test
+    public void testNOT() {
+        assertEquals("T", logica.interpretar("(NOT NIL)")); // NOT de NIL es T
+        assertEquals("NIL", logica.interpretar("(NOT 1)")); // NOT de 1 es NIL
+        assertEquals("NIL", logica.interpretar("(NOT 0)")); // NOT de 0 (según implementación)
+        assertEquals("T", logica.interpretar("(NOT (OR NIL NIL))")); // NOT de expresión
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNOTConVariosParametros() {
+        logica.interpretar("(NOT 1 2)"); // NOT solo permite un argumento
+    }
+}
