66105c5abcb2ed0b17d654e374ad9cc35d1b4b63
KristCg <136038238+KristCg@users.noreply.github.com>
2025-03-24 01:27:13 -0600

    Test de AritmeticasLisp

---
+++ demo/src/test/java/proyecto/AritméticasLispTest.java
@@ -0,0 +1,31 @@
+package proyecto;
+
+import org.junit.Test;
+inoort static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+public class AritmeticasLispTest {
+    @Test
+    public void testOperacionesAritmeticas() {
+        Tokenizer tokenizer = new Tokenizer();
+        AritmeticasLisp aritmeticas = new AritmeticasLisp(tokenizer);
+
+        assertEquals(15, aritmeticas.calcularOperacion("+", Arrays.asList(10, 5)));
+        assertEquals(5, aritmeticas.calcularOperacion("-", Arrays.asList(10, 5)));
+        assertEquals(50, aritmeticas.calcularOperacion("*", Arrays.asList(10, 5)));
+        assertEquals(2, aritmeticas.calcularOperacion("/", Arrays.asList(10, 5)));
+    }
+
+    @Test
+    public void testDivisionPorCero() {
+        Tokenizer tokenizer = new Tokenizer();
+        AritmeticasLisp aritmeticas = new AritmeticasLisp(tokenizer);
+
+        Exception exception = assertThrows(ArithmeticException.class, () -> {
+            aritmeticas.calcularOperacion("/", Arrays.asList(10, 0));
+        });
+
+        assertTrue(exception.getMessage().contains("División por cero"));
+    }
+}
